dbcfe91e5340af336a9528cf1ba7729420d4f444
Willian Boaventura <williandosanjos14@gmail.com>
2026-05-18 14:38:35 -0300

    Commiti inicial projeto Maralis

---
+++ src/main/java/com/silmara/maralis/MaralisApplication.java
@@ -0,0 +1,13 @@
+package com.silmara.maralis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaralisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaralisApplication.class, args);
+	}
+
+}
